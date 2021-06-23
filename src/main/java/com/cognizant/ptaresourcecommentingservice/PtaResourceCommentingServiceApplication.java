fd65841ee2964dedc56d3230fd2e2ac28eb333b5
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2021-06-22 23:42:43 -0400

    Initial Commit

---
+++ src/main/java/com/cognizant/ptaresourcecommentingservice/PtaResourceCommentingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.ptaresourcecommentingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtaResourceCommentingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtaResourceCommentingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/ptaresourcecommentingservice/PtaResourceCommentingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.ptaresourcecommentingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtaResourceCommentingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
