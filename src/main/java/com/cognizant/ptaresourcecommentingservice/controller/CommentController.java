ed86021b61736ae940441b51ec1b200173fb94d5
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2021-06-23 15:20:43 -0400

    fixed the missing endpoints

--- src/main/java/com/cognizant/ptaresourcecommentingservice/controller/CommentController.java
+++ src/main/java/com/cognizant/ptaresourcecommentingservice/controller/CommentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/comment")
 public class CommentController {
     @Autowired
     CommentRepository commentRepo;
